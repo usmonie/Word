c1921fe6a1adcbaa90ee8f87172a58b813c82ef7
usmanakhmedov <nie@usmonie.com>
2024-06-24 23:31:27 +0200

    Improves

--- composeApp/src/commonMain/kotlin/com/usmonie/word/App.kt
+++ composeApp/src/commonMain/kotlin/com/usmonie/word/App.kt
@@ -16,32 +16,35 @@ import com.usmonie.word.features.ads.ui.LocalAdsManager
 import com.usmonie.word.features.settings.domain.models.DarkThemeMode
 import com.usmonie.word.features.settings.ui.models.UserSelectedTheme
 import com.usmonie.word.features.settings.ui.usecases.UserSelectedThemeUseCase
+import kotlinx.coroutines.Dispatchers
 import org.koin.compose.koinInject
 
 @Composable
 fun App(
-    adsManager: AdsManager = koinInject(),
-    routeManager: RouteManager = koinInject(),
-    userSelectedThemeUseCase: UserSelectedThemeUseCase = koinInject(),
+	adsManager: AdsManager = koinInject(),
+	routeManager: RouteManager = koinInject(),
+	userSelectedThemeUseCase: UserSelectedThemeUseCase = koinInject(),
 ) {
-    val isSystemInDark = isSystemInDarkTheme()
-    val currentTheme by userSelectedThemeUseCase().collectAsState(
-        UserSelectedTheme(
-            WordThemes.DEEP_INDIGO,
-            ModernChic,
-            DarkThemeMode.NEVER
-        )
-    )
+	val isSystemInDark = isSystemInDarkTheme()
+	val currentTheme by userSelectedThemeUseCase().collectAsState(
+		UserSelectedTheme(
+			WordThemes.DEEP_INDIGO,
+			ModernChic,
+			DarkThemeMode.AUTO
+		),
+		Dispatchers.Main
+	)
 
-    val useDarkThemeMode = currentTheme.darkThemeMode == DarkThemeMode.ALWAYS ||
-        (currentTheme.darkThemeMode == DarkThemeMode.AUTO && isSystemInDark)
-    WordTheme(
-        currentTheme.themes,
-        currentTheme.typography,
-        useDarkThemeMode
-    ) {
-        CompositionLocalProvider(LocalAdsManager provides adsManager) {
-            Root(routeManager, isGestureNavigationEnabled = true)
-        }
-    }
+	val useDarkThemeMode = currentTheme.darkThemeMode == DarkThemeMode.ALWAYS ||
+		(currentTheme.darkThemeMode == DarkThemeMode.AUTO && isSystemInDark)
+
+	WordTheme(
+		currentTheme.themes,
+		currentTheme.typography,
+		useDarkThemeMode
+	) {
+		CompositionLocalProvider(LocalAdsManager provides adsManager) {
+			Root(routeManager, isGestureNavigationEnabled = true)
+		}
+	}
 }
